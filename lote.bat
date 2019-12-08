38b1aa88438a2636278997a6640c9ab766df0b1b
Lewis Palacios Gonzales <lpalaciosg@unprg.edu.pe>
2019-12-07 19:48:34 -0500

    condicional_doble_12

--- lote.bat
+++ lote.bat
@@ -31,5 +31,8 @@ python palacios/simple_10.py 1325
 python palacios/doble_11.py 100 100 100 100
 python palacios/doble_11.py 90 90 80 90
 python palacios/doble_11.py 10 50 20 60
+python palacios/doble_12.py "admin" "$#r"
+python palacios/doble_12.py "cuenta" "incorrecta"
+python palacios/doble_12.py "3215" "65152"
 
 
