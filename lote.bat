7eb52bb638d1c20e4a13177ff371e73c90cddcda
Lewis Palacios Gonzales <lpalaciosg@unprg.edu.pe>
2019-12-07 19:12:17 -0500

    condicional_simple_03

--- lote.bat
+++ lote.bat
@@ -4,3 +4,8 @@ python palacios/simple_01.py 10 50 20 60
 python palacios/simple_02.py "admin" "$#r"
 python palacios/simple_02.py "cuenta" "incorrecta"
 python palacios/simple_02.py "3215" "65152"
+python palacios/simple_03.py 85 79
+python palacios/simple_03.py 65 52
+python palacios/simple_03.py 90 88
+
+
